11ab2fef932b50a8a9692e44fb1137d41de26927
danneh3826 <dan@danneh.org>
2012-08-13 21:58:47 +0100

    added file modification time checker to prevent caching

--- View/Helper/ServerUrl.php
+++ View/Helper/ServerUrl.php
@@ -2,11 +2,23 @@
 
 class BTS_View_Helper_ServerUrl extends Zend_View_Helper_ServerUrl {
     
-    public function serverUrl($requestUri = null) {
+    public function serverUrl($requestUri = null, $filepath = null) {
+        if (is_null($requestUri) || substr($requestUri, 0, 1) != "/") {
+            $requestUri = "/" . $requestUri;
+        }
         $url = parent::serverUrl($requestUri);
         
-        $filepath = dirname(APPLICATION_PATH) . "/public" . $requestUri;
-        if (file_exists($filepath)) {
+        if (is_null($filepath)) {
+            $filepath = dirname(APPLICATION_PATH) . "/public" . $requestUri;
+        }
+        else {
+            $filepath = dirname(APPLICATION_PATH) . "/public" . $filepath;
+        }
+        
+        // $requestUri should be at least something. null means homepage.
+        // file_exists will always be true, and we don't want an m= query
+        // string on links back to the homepage...
+        if ($requestUri != "/" && file_exists($filepath)) {
             $url .= "?m=" . filemtime($filepath);
         }
         
