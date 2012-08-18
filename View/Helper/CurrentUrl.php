810b75db9f0e4420f9ed280f9b1c643b6de76316
danneh3826 <dan@danneh.org>
2012-08-18 22:24:11 +0100

    added ability to base64 encode

--- View/Helper/CurrentUrl.php
+++ View/Helper/CurrentUrl.php
@@ -4,12 +4,17 @@ class BTS_View_Helper_CurrentUrl
     extends Zend_View_Helper_Abstract
 {
     
-    public function currentUrl()
+    public function currentUrl($base64 = false)
     {
         $fc = Zend_Controller_Front::getInstance();
         $request = $fc->getRequest();
         $url = $request->getServer("REQUEST_URI");
-        return $url;
+        if ($base64) {
+            return base64_encode($url);
+        }
+        else {
+            return $url;
+        }
     }
     
 }
