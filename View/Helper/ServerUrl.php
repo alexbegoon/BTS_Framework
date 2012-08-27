8a7bafa57e2fbc5d0d272ac5696c594490b1be18
danneh3826 <dan@danneh.org>
2012-08-27 21:15:21 +0100

    add anticache parameter to optionally exclude ?m= query string

--- View/Helper/ServerUrl.php
+++ View/Helper/ServerUrl.php
@@ -2,7 +2,7 @@
 
 class BTS_View_Helper_ServerUrl extends Zend_View_Helper_ServerUrl {
     
-    public function serverUrl($requestUri = null, $filepath = null) {
+    public function serverUrl($requestUri = null, $filepath = null, $antiCache = true) {
         if (is_null($requestUri) || substr($requestUri, 0, 1) != "/") {
             $requestUri = "/" . $requestUri;
         }
@@ -18,7 +18,7 @@ class BTS_View_Helper_ServerUrl extends Zend_View_Helper_ServerUrl {
         // $requestUri should be at least something. null means homepage.
         // file_exists will always be true, and we don't want an m= query
         // string on links back to the homepage...
-        if ($requestUri != "/" && file_exists($filepath)) {
+        if ($requestUri != "/" && file_exists($filepath) && $antiCache) {
             $url .= "?m=" . filemtime($filepath);
         }
         
