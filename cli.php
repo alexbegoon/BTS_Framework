fc8c0a5dff42f8329e23757b96d1c17f159421b4
danneh3826 <dan@danneh.org>
2012-11-09 09:52:00 +0000

    add environment flag to cli.php, run zend_application bootstrap before firing controller

--- cli.php
+++ cli.php
@@ -2,34 +2,35 @@
 
 date_default_timezone_set('Europe/London');
 
-define("APPLICATION_ENV", "production");
+define("ROOT_DIR", dirname($_SERVER['SCRIPT_FILENAME']));
 
 error_reporting(E_ALL|E_STRICT);
 ini_set('display_errors',true);
 
 // Define path to application directory
 defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH', dirname($_SERVER['SCRIPT_FILENAME']) . '/application');
+    || define('APPLICATION_PATH', ROOT_DIR . '/application');
 
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
-    realpath(dirname($_SERVER['SCRIPT_FILENAME']) . '/library'),
+    realpath(dirname(ROOT_DIR) . '/library'),
     get_include_path(),
 )));
 
-require_once('Zend/Loader/Autoloader.php');
-// Should probably take this from the main config file...
-Zend_Loader_Autoloader::getInstance()->registerNamespace("App_");
-Zend_Loader_Autoloader::getInstance()->registerNamespace("BTS_");
-Zend_Loader_Autoloader::getInstance()->registerNamespace("Zend_");
+//require_once('Zend/Loader/Autoloader.php');
+//// Should probably take this from the main config file...
+//Zend_Loader_Autoloader::getInstance()->registerNamespace("App_");
+//Zend_Loader_Autoloader::getInstance()->registerNamespace("BTS_");
+//Zend_Loader_Autoloader::getInstance()->registerNamespace("Zend_");
 
 try {
+    include_once "Zend/Console/Getopt.php";
     $opts = new Zend_Console_Getopt(
         array(
             'help|h' => 'Displays usage information.',
             'action|a=s' => 'Action to perform in format of module.controller.action',
             'verbose|v' => 'Verbose messages will be dumped to the default output.',
-            'development|d' => 'Enables development mode.',
+            'env|e=s' => 'Set environment mode (defaults to "production").',
             'params|p=s' => 'Parameters to controller.',
         )
     );
@@ -44,7 +45,24 @@ if (isset($opts->h)) {
     exit;
 }
 
+if (isset($opts->e)) {
+    define("APPLICATION_ENV", $opts->e);
+}
+else {
+    define("APPLICATION_ENV", "production");
+}
+
 if(isset($opts->a)) {
+    /** Zend_Application */
+    require_once 'Zend/Application.php';
+
+    // Create application, bootstrap, and run
+    $application = new Zend_Application(
+        APPLICATION_ENV,
+        APPLICATION_PATH . '/configs/application.ini'
+    );
+    $application->bootstrap();
+    
     $reqRoute = array_reverse(explode('.', $opts->a));
     list($action, $controller, $module) = $reqRoute;
     
