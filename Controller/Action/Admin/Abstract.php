ddf3b8a7ebcb1a2f4af50875637b619ebb46179b
Dan Jones <dan@danneh.org>
2013-06-24 20:57:22 +0100

    explicit index action in login route

--- Controller/Action/Admin/Abstract.php
+++ Controller/Action/Admin/Abstract.php
@@ -2,6 +2,6 @@
 
 abstract class BTS_Controller_Action_Admin_Abstract extends BTS_Controller_Action_Abstract {
     protected $_requiresAuth = true;
-    protected $_loginRoute = "/admin/auth";
+    protected $_loginRoute = "/admin/auth/index";
     protected $_authStorage = "Admin_Auth";
 }
